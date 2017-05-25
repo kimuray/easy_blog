394d54aac6cf048554dbb007c0b1a8dbc073774a
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2017-05-25 23:30:14 +0900

    First commit!

---
+++ config/application.rb
@@ -0,0 +1,15 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+Bundler.require(*Rails.groups)
+
+module ColBlog
+  class Application < Rails::Application
+    config.load_defaults 5.1
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
+    config.active_record.default_timezone = :local
+    config.active_record.raise_in_transactional_callbacks = true
+  end
+end
